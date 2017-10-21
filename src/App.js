926b2e5557e0b7d4c369aef0ac9825ff4a772ec6
KONDO-yuuki <efb.mzw.yk@gmail.com>
2017-10-21 11:14:04 +0900

    1st commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+console.log('Thank you my best friends!')
+export default App;
